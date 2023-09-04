f15408331255c3416e39f3ba2b22584aebf33d96
Kazi Afzaluddin <kazipintu@gmail.com>
2023-09-05 03:24:40 +0530

    Java Script Practice 05092023

---
+++ JavaScript/JSPractice.js
@@ -0,0 +1,46 @@
+price = 10
+// here, price is "variable", "10" is data
+
+cost = 20
+// here, cost is "variable", "20" is data
+
+value= 30
+// here, value is "variable", "10" is data
+
+document.write(price)
+// output can be written in three different ways, (1) document.write(variable), we can see it on the body of the browser  
+
+alert(cost)
+// (2) alert(varibale),it generates alert, it is used for debugging
+
+console.log(value)
+// mmost frequently used (3) console.log(varibale), it can be seen in inspect console ... any error can be seen there ... also it can be edited/typed ... we can write codes on console also ... we can see our entire code in resources ... etc.
+
+var age = 90
+console.log(typeof age)
+// "typeof" is written to write type of output data, here it is "number"
+
+var name1 = 'kazi'
+var name2 = "abdullah"
+var name3 = "1003"
+var sentense= "Rahim is a good boy"
+console.log(typeof name1)
+console.log(typeof name2)
+console.log(typeof name3)
+console.log(typeof sentense)
+
+// anything written between single quotation (' ') or double quotation (" ") is called a string type data ... even if we store number within quotations, number will  be defined as string ...  we use number for arithmetic operator
+
+remainder = 100 % 7
+console.log(remainder)
+// Here % is called modulous/modulo, it calculates the remainder..
+
+power1 = Math.pow(10, 3)
+power2 = Math.pow(81, .25)
+power3 = Math.pow(125, (1/3))
+
+console.log(power1)
+console.log(power2)
+console.log(power3)
+
+
